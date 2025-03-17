11c15709b7e7f7ebafe9e91d02896683c6f9a75b
Jacob Julius <jeccopatrick@outlook.com>
2025-03-17 15:27:16 +0300

    Add TypeScript interfaces for document and layout components

---
+++ app/types/root.ts
@@ -0,0 +1,12 @@
+export interface IDocumentProps {
+    children?: any;
+    title?: string;
+}
+
+export interface ILayoutProps {
+    children?: any;
+}
+
+export interface IErrorProps {
+    error: Error
+}
